bfd802e52e8ab39a312162d3b59b8dd8c893af74
Neorodu <knur13533@gmail.com>
2026-06-21 21:40:28 +0700

    Change audio link for 'Mimpi malam' album
    Updated audio source for the first album in the playlist.

--- script-music.js
+++ script-music.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  // ==================== HAMBURGER MENU + TOMBOL X ====================
+  // ====================  ====================
   const hamburger = document.getElementById('hamburger');
   const mobileMenu = document.getElementById('mobile-menu');
 
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
       mobileMenu.classList.toggle('hidden');
     });
 
-    // Tombol Close (X) di dalam menu
+  
     const closeBtn = document.createElement('button');
     closeBtn.className = "absolute top-6 right-6 text-4xl text-zinc-400 hover:text-white";
     closeBtn.innerHTML = '×';
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // ==================== PLAYLIST ALBUM ====================
   const albums = [
-    { id: 1, title: "Mimpi malam", artist: "gemagus", img: "https://picsum.photos/400/400?random=1", audio: "https://archive.org/download/musik-ajaib-gunakan/MUSIK%20AJAIB%20GUNAKAN%20HEADSET%20%20DJ%[...]" },
+    { id: 1, title: "Mimpi malam", artist: "gemagus", img: "https://picsum.photos/400/400?random=1", audio: "https://archive.org/download/a-2-pendekar-3-x-adtuying-mashup/A2%20-%20pendekar3%20x%20ad%2Ctuying%20%28Mashup%29.mp3" },
     { id: 2, title: "intrument", artist: "gamagus", img: "https://picsum.photos/400/400?random=4", audio: "https://archive.org/download/iringan-2/Iringan2.mp3" },
     { id: 3, title: "gemagus", artist: "flute", img: "https://picsum.photos/400/400?random=3", audio: "https://archive.org/download/epic-games_202604/Epic%20Games.mp3" },
     { id: 4, title: "Lost in Valley", artist: "gemagus", img: "https://picsum.photos/id/1016/400/400", audio: "https://archive.org/download/epic-games_202604/Gms%20film.mp3" },
