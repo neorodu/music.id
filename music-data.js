b4282cf59dff9027f52b044a005033d89fd70459
Neorodu <knur13533@gmail.com>
2026-07-02 20:45:16 +0700

    Add files via upload

---
+++ music-data.js
@@ -0,0 +1,102 @@
+/* =========================================================
+   FLUTEMUSIC — data file
+   Ganti nilai di bawah ini dengan konten musik sendiri:
+   - MUSICIANS  -> slide untuk hero slideshow
+   - TRACKS     -> daftar lagu di tracklist & pemutar
+
+   Catatan:
+   gabut seumur hidup
+   ========================================================= */
+
+const MUSICIANS = [
+  {
+    eyebrow: "SESI // 01",
+    title: "Di Balik endog",
+    desc: "Proses produksi malam hari pribadi.",
+    image: "https://picsum.photos/id/1016/400/400"
+  },
+  {
+    eyebrow: "SESI // 02",
+    title: "Live ",
+    desc: "Rekaman flute .",
+    image: "https://picsum.photos/400/400?random=4"
+  },
+  {
+    eyebrow: "SESI // 03",
+    title: "Perjalanan Mencari Nada",
+    desc: "musik FLUTEMUSIC.",
+    image: "https://picsum.photos/400/400?random=1"
+  },
+  {
+    eyebrow: "SESI // 04",
+    title: "gunung",
+    desc: "Momen tampil langsung — energi yang selalu jadi alasan untuk terus membuat musik.",
+   image: "https://picsum.photos/400/400?random=3"
+  }
+];
+
+const TRACKS = [
+  {
+    title: "gemagus",
+    artist: "FLUTEMUSIC",
+    tag: "belajar",
+    cover: "https://picsum.photos/id/1062/300/300",
+    src: "https://archive.org/download/a-1gemagus-x-adtuying-imam/A1gemagus%20x%20ad%2Ctuying%20%28imam%29.mp3"
+  },
+  {
+    title: " gemagas",
+    artist: "FLUTEMUSIC",
+    tag: "meneng",
+    cover: "https://picsum.photos/id/1074/300/300",
+    src: "https://archive.org/download/a-1gemagus-x-adtuying-imam/Gemagus%28imam%29.mp3"
+  },
+  {
+    title: "gemagas Heart",
+    artist: "FLUTEMUSIC",
+    tag: "maen",
+    cover: "https://picsum.photos/id/110/300/300",
+    src: "https://archive.org/download/a-1gemagus-x-adtuying-imam/a%2C%28imam%29sergmen.mp3"
+  },
+  {
+    title: "gemagus",
+    artist: "udan",
+    tag: "Ambient",
+    cover: "https://picsum.photos/id/145/300/300",
+    src: "https://archive.org/download/udan-di-desa/hujan%20gerimis%20suara%20katak%20dan%20jangkrik%20suasana%20desa%20dijamin%20langsung%20tidur%20-%20Dunia%20Relaksasi.mp3"
+  },
+  {
+    title: "main lah",
+    artist: "udan rain",
+    tag: "hujan",
+    cover: "https://picsum.photos/id/160/300/300",
+    src: "https://archive.org/download/raingueudan/rain%28gueudan%29.mp3"
+  },
+  {
+    title: "music5",
+    artist: "FLUTEMUSIC",
+    tag: "Instrumental",
+    cover: "https://picsum.photos/id/177/300/300",
+    src: "https://archive.org/download/a-2-pendekar-3-x-adtuying-mashup/A2%20-%20pendekar3%20x%20ad%2Ctuying%20%28Mashup%29.mp3"
+  },
+    {
+    title: "main lah",
+    artist: "udan rain",
+    tag: "huja",
+    cover: "https://picsum.photos/id/160/300/300",
+    src: "https://archive.org/download/a-1gemagus-x-adtuying-imam/A1gemagus%20x%20ad%2Ctuying%20%28imam%29.mp3"
+  },
+    {
+    title: "music6",
+    artist: "FLUTEMUSIC",
+    tag: "Instrumental",
+    cover: "https://picsum.photos/id/177/300/300",
+    src: "https://archive.org/download/iringan-2/Iringan2.mp3"
+  },
+    {
+    title: "flute",
+    artist: "gemagus",
+    tag: "gabut",
+    cover: "https://picsum.photos/id/160/300/300",
+    src: "https://archive.org/download/a-1gemagus-x-adtuying-imam/Gemagus%28imam%29.mp3"
+  },
+];
